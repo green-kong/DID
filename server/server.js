855aeeabbef71a73d4466ac5bdba9205f543d6a6
dev_kong <ahdjd5@gmail.com>
2022-08-24 11:13:19 +0900

    [back] fix : coflict error

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const userRouter = require("./routes/user.js");
+const cors = require('cors');
+const path = require('path');
 
+const devRouter = require('./routers/dev.router.js');
+const userRouter = require('./routes/user.js');
 
 app.use(express.static(path.join(__dirname, './upload')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 app.use(
   cors({
     origin: ['http://localhost:3000'],
@@ -18,7 +20,6 @@ app.use(
 
 app.use(userRouter);
 app.use('/dev', devRouter);
-
 
 app.listen(4000, () => {
   console.log('back server port 4000');
