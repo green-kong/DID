e6a19f193d3143b8de854edd6fed176acbd44227
dev_kong <ahdjd5@gmail.com>
2022-08-25 09:08:40 +0900

    [front] fix : appList page useContext update

--- client/api/dev/appList.ts
+++ client/api/dev/appList.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const getAppList = async (userIdx: number) => {
+const getAppList = async (userIdx: string) => {
   const url = 'http://localhost:4000/dev/appList';
   const body = { _id: userIdx };
   const response = await axios.post(url, body);
