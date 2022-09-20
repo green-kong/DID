92abd777d945da1182aa9132267202a1182ea163
승주 <seungju@seungjuui-MacBookAir.local>
2022-09-20 11:26:56 +0900

    fix: merge-conflict

---
+++ client/.next/server/chunks/482.js
@@ -0,0 +1,64 @@
+"use strict";
+exports.id = 482;
+exports.ids = [482];
+exports.modules = {
+
+/***/ 8237:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "WT": () => (/* binding */ ConnectionsWrap),
+/* harmony export */   "bm": () => (/* binding */ ConnectionInfo),
+/* harmony export */   "cw": () => (/* binding */ ConnectedDiv),
+/* harmony export */   "dk": () => (/* binding */ ConnectionImg)
+/* harmony export */ });
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7518);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_0__);
+
+const ConnectionsWrap = /*#__PURE__*/styled_components__WEBPACK_IMPORTED_MODULE_0___default().div.withConfig({
+  displayName: "connections__ConnectionsWrap",
+  componentId: "sc-8jbnes-0"
+})(["width:100%;height:100%;display:flex;flex-wrap:wrap;gap:5%;"]);
+const ConnectedDiv = /*#__PURE__*/styled_components__WEBPACK_IMPORTED_MODULE_0___default().div.withConfig({
+  displayName: "connections__ConnectedDiv",
+  componentId: "sc-8jbnes-1"
+})(["width:30%;height:240px;background-color:#fff;margin-bottom:40px;border-radius:8px;padding:20px;box-sizing:border-box;display:flex;"]);
+const ConnectionImg = /*#__PURE__*/styled_components__WEBPACK_IMPORTED_MODULE_0___default().div.withConfig({
+  displayName: "connections__ConnectionImg",
+  componentId: "sc-8jbnes-2"
+})(["width:50%;display:flex;justify-content:center;align-items:center;"]);
+const ConnectionInfo = /*#__PURE__*/styled_components__WEBPACK_IMPORTED_MODULE_0___default().div.withConfig({
+  displayName: "connections__ConnectionInfo",
+  componentId: "sc-8jbnes-3"
+})(["padding:40px 0;width:50%;& > .connection_name{font-size:20px;font-weight:600;margin-bottom:16px;color:black;}& > .connection_desc{color:#c2bdba;font-size:12px;margin-bottom:30px;}& > .disconnect_btn{width:80px;height:30px;background-color:#f44954;border:none;border-radius:4px;color:white;}"]);
+
+/***/ }),
+
+/***/ 5485:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Dx": () => (/* binding */ Title),
+/* harmony export */   "wW": () => (/* binding */ ContentTitle),
+/* harmony export */   "zg": () => (/* binding */ TitleIcon)
+/* harmony export */ });
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7518);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_0__);
+
+const ContentTitle = /*#__PURE__*/styled_components__WEBPACK_IMPORTED_MODULE_0___default().div.withConfig({
+  displayName: "title__ContentTitle",
+  componentId: "sc-1fjmapq-0"
+})(["width:210px;height:96px;margin:0 auto;display:flex;flex-direction:column;justify-content:space-between;align-items:center;margin-bottom:45px;"]);
+const TitleIcon = /*#__PURE__*/styled_components__WEBPACK_IMPORTED_MODULE_0___default().div.withConfig({
+  displayName: "title__TitleIcon",
+  componentId: "sc-1fjmapq-1"
+})(["width:50px;height:50px;background-image:url(", ");background-size:50px;"], props => props.imageUrl);
+const Title = /*#__PURE__*/styled_components__WEBPACK_IMPORTED_MODULE_0___default().p.withConfig({
+  displayName: "title__Title",
+  componentId: "sc-1fjmapq-2"
+})(["font-size:30px;color:white;"]);
+
+/***/ })
+
+};
+;
